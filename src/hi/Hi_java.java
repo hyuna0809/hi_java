2f76b09b744bc1df9fcb39540faf82351d9b7e2c
hyuna0809 <hyuna1704@naver.com>
2023-08-07 12:01:35 +0900

    성공?

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -5,7 +5,7 @@ public class Hi_java {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("성공했네~~~ 기쁘네");
-		System.out.println("성공?");
+		System.out.println("성공????");
 	}
 
 }
