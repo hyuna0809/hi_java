0ab1604a8d5755de6269730e5929a943d7027634
hyuna0809 <hyuna1704@naver.com>
2023-08-07 12:29:48 +0900

    777

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -5,7 +5,7 @@ public class Hi_java {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("안녕하세요");
-		System.out.println("반갑습니다.");
+		System.out.println("반갑습니다123.");
 	}
 
 }
